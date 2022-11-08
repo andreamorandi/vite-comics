e374bfd9632a1826d902ba7e1254294f72c7532d
andreamorandi <morandiandrearocco@gmail.com>
2022-11-08 14:38:04 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppContent from "./components/AppContent.vue";
+import AppBanner from "./components/AppBanner.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  components: {
+    AppHeader,
+    AppContent,
+    AppBanner,
+    AppFooter
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <AppContent />
+  <AppBanner />
+  <AppFooter />
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
---
+++ src/components/AppBanner.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+        name: "AppBanner"
+    }
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
